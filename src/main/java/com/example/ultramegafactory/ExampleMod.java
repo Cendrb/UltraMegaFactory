d47453a957d6030b04738c434894b7e88e4f9766
Čeněk Drbal <cendrb@gmail.com>
2016-03-24 19:07:50 +0100

    johncena

---
+++ src/main/java/com/example/ultramegafactory/ExampleMod.java
@@ -0,0 +1,20 @@
+package com.example.ultramegafactory;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "ultramegafactory";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
