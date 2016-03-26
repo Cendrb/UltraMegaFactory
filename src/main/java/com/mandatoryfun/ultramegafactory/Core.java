e217d8020fccba3074188502e77e344dc0d73434
WesterWest <westerofwest@gmail.com>
2016-03-26 11:21:25 +0100

    dasd
    dadad

--- src/main/java/com/mandatoryfun/ultramegafactory/Core.java
+++ src/main/java/com/mandatoryfun/ultramegafactory/Core.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = RefStrings.MODID, version = RefStrings.VERSION, name = RefStrings.NAME)
-public class ExampleMod {
+public class Core {
     public static final String MODID = "ultramegafactory";
     public static final String VERSION = "1.0";
 
@@ -18,7 +18,7 @@ public class ExampleMod {
     public static CommonProxy proxy;
 
     @Mod.Instance
-    public static ExampleMod instance = new ExampleMod();
+    public static Core instance = new Core();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent e) {
