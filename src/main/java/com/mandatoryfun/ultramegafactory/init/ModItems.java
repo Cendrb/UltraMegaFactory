45d2644d3a6e030235c59933d17ffe3d65dee4e9
Čeněk Drbal <cendrb@gmail.com>
2016-03-27 11:21:07 +0200

    retgister method

--- src/main/java/com/mandatoryfun/ultramegafactory/init/ModItems.java
+++ src/main/java/com/mandatoryfun/ultramegafactory/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.mandatoryfun.ultramegafactory.init;
 
 import com.mandatoryfun.ultramegafactory.item.ItemGeneric;
+import com.mandatoryfun.ultramegafactory.item.ItemIngotGeneric;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -16,6 +17,12 @@ public final class ModItems {
     private static ItemGeneric register(String unlocalizedName) {
         ItemGeneric item;
         GameRegistry.registerItem(item = new ItemGeneric(unlocalizedName), unlocalizedName);
+        return item;
+    }
+
+    private static ItemIngotGeneric register(String unlocalizedName, String formula) {
+        ItemIngotGeneric item;
+        GameRegistry.registerItem(item = new ItemIngotGeneric(unlocalizedName, formula), unlocalizedName);
         return item;
     }
 }
